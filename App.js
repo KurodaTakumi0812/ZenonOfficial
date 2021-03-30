2507f76429c690b000755a16787b8cee04c45006
DarkRo <63647187+KurodaTakumi0812@users.noreply.github.com>
2021-03-31 05:22:09 +0900

    update

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { LogBox } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { Alert, LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View } from 'react-native';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import firebase from 'firebase';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+
 
 import NotificationDetailScreen from './src/screens/NotificationDetailScreen';
 import MainDrawerNavigator from './src/screens/MainDrawerNavigator';
@@ -16,6 +17,14 @@ import InquiriesScreen from './src/screens/InquiriesScreens';
 import SettingsScreen from './src/screens/SettingsScreen';
 
 import { firebaseConfig } from './env';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
@@ -26,7 +35,71 @@ LogBox.ignoreLogs(['Setting a timer']);
 
 const Stack = createStackNavigator();
 
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
+
+const saveDeviceToken = async (token) => {
+  try {
+    const db = firebase.firestore();
+    const docRef = await db.collection('tokens').add({ token })
+  } catch (err) { }
+};
+
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => {
+      setExpoPushToken(token)
+      Alert.alert(expoPushToken);
+      saveDeviceToken(token);
+    });
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
